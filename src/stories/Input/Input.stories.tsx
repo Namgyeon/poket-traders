aa6b30cd150082e53e2c2aac51fe0cacf4818205
Namgyeon <getam101@naver.com>
2025-08-19 17:38:03 +0900

    빌드오류 해결 : RHF error 속성 수정

--- src/stories/Input/Input.stories.tsx
+++ src/stories/Input/Input.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from "@storybook/react";
+import type { Meta, StoryObj } from "@storybook/nextjs-vite";
 import Input from "@/components/ui/Input/Input";
 
 const meta: Meta<typeof Input> = {
