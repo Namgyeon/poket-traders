5f72dc3d569034c1cac19473643278a10327e809
Namgyeon <getam101@naver.com>
2025-08-20 17:43:30 +0900

    feat: 유저정보 get 리액트쿼리 구현

--- src/apis/auth/queries.ts
+++ src/apis/auth/queries.ts
@@ -1,6 +1,7 @@
-import { useMutation } from "@tanstack/react-query";
-import { signin, signinWithGoogle, signup } from "@/apis/auth";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { getUser, signin, signinWithGoogle, signup } from "@/apis/auth";
 import { SigninFormRequest, SignupFormRequest, User } from "@/apis/auth/types";
+import { auth } from "@/lib/firebase";
 
 export function useSignup() {
   return useMutation<User, Error, SignupFormRequest>({
@@ -17,5 +18,13 @@ export function useSignin() {
 export function useSigninWithGoogle() {
   return useMutation<User, Error, void>({
     mutationFn: signinWithGoogle,
+  });
+}
+
+export function useGetUser() {
+  return useQuery<User, Error>({
+    queryKey: ["user"],
+    queryFn: getUser,
+    enabled: !!auth.currentUser,
   });
 }
