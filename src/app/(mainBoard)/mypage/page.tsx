73dd1bbb2e82ca3efaed34d31be234b7aa6ab3e0
Namgyeon <getam101@naver.com>
2025-09-22 15:15:21 +0900

    Feat(#39): AuthGuard, 내가 작성한글 배경색 추가. (#40)
    * feat: AuthGuard컴포넌트 구현. 로그인해야 mypage 접근할 수 있도록 설정
    
    * style: 내가 작성한글은 배경색을 다르게 표시

--- src/app/(mainBoard)/mypage/page.tsx
+++ src/app/(mainBoard)/mypage/page.tsx
@@ -1,13 +1,16 @@
+import AuthGuard from "@/components/auth/AuthGuard";
 import MyCommentList from "@/components/mypage/MyCommentList";
 import MyTradeCardList from "@/components/mypage/MyTradeCardList";
 import UserInfoUpdateForm from "@/components/mypage/UserInfoUpdateForm";
 
 export default function MyPage() {
   return (
-    <div className="space-y-10">
-      <UserInfoUpdateForm />
-      <MyTradeCardList />
-      <MyCommentList />
-    </div>
+    <AuthGuard redirectTo="/signin">
+      <div className="space-y-10">
+        <UserInfoUpdateForm />
+        <MyTradeCardList />
+        <MyCommentList />
+      </div>
+    </AuthGuard>
   );
 }
